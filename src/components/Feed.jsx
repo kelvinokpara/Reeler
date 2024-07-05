fc71717ca767e4b6b26379a9808b59597bf8e1b3
unknown <okparakelvin65@gmail.com>
2024-07-05 17:27:40 +0100

    sidebar and video section update

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,7 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
+
+import { Sidebar, Videos } from "./";
 
 const Feed = () => {
   return (
@@ -10,7 +12,29 @@ const Feed = () => {
           borderRight: "1px solid #3d3d3d",
           px: { sx: 0, md: 2 },
         }}
-      ></Box>
+      >
+        <Sidebar />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff " }}
+        >
+          copyright 2024 reeler
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New <span style={{ color: "#FC1503" }}>videos</span>
+        </Typography>
+        {/* videos component */}
+        <Videos videos={[]} />
+      </Box>
     </Stack>
   );
 };
